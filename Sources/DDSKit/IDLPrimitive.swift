0655a0692e01a88049665b5541607e084e394678
Hunter Baker <hunterbaker@me.com>
2024-08-07 20:59:04 -0400

    Rename IDLType to IDLPrimitive

---
+++ Sources/DDSKit/IDLPrimitive.swift
@@ -0,0 +1,17 @@
+import CxxStdlib
+
+public protocol IDLPrimitive: Hashable {}
+
+extension Int8: IDLPrimitive {}
+extension UInt8: IDLPrimitive {}
+extension Int16: IDLPrimitive {}
+extension UInt16: IDLPrimitive {}
+extension Int32: IDLPrimitive {}
+extension UInt32: IDLPrimitive {}
+extension Int64: IDLPrimitive {}
+extension UInt64: IDLPrimitive {}
+extension Float: IDLPrimitive {}
+extension Double: IDLPrimitive {}
+extension Float80: IDLPrimitive {}
+extension Bool: IDLPrimitive {}
+extension std.string: IDLPrimitive {}
